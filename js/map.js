b187db85279f2d4b0d1c4219d6aa6f866f34d179
Will Klein <will.klein@gmail.com>
2013-04-27 01:09:01 -0400

    Move center point of map.

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var map;
 
 (function() {
     var options = {
-        center: new L.LatLng(42.357688, -71.073518),
+        center: new L.LatLng(42.358132, -71.093431),
         zoom: 14,
         minZoom: 14
     };
